b86404e7237e5e50c9aaeee3400b455a026492be
Chris <cristianocorona2424@gmail.com>
2025-12-09 11:00:30 +0000

    file refactoring. Step37 example moved in a dedicated file. Useless libraries removed

--- src/ADROperator.cpp
+++ src/ADROperator.cpp
@@ -1,36 +1,5 @@
 #include "ADROperator.hpp"
 
-//Libraries for dealii
-
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/function.h>
-#include <deal.II/base/timer.h>
- 
-#include <deal.II/lac/affine_constraints.h>
-#include <deal.II/lac/solver_cg.h>
-#include <deal.II/lac/la_parallel_vector.h>
-#include <deal.II/lac/precondition.h>
- 
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/mapping_q1.h>
- 
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/grid_generator.h>
- 
-#include <deal.II/multigrid/multigrid.h>
-#include <deal.II/multigrid/mg_transfer_matrix_free.h>
-#include <deal.II/multigrid/mg_tools.h>
-#include <deal.II/multigrid/mg_coarse.h>
-#include <deal.II/multigrid/mg_smoother.h>
-#include <deal.II/multigrid/mg_matrix.h>
- 
-#include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/vector_tools.h>
- 
-#include <deal.II/matrix_free/matrix_free.h>
-#include <deal.II/matrix_free/operators.h>
-#include <deal.II/matrix_free/fe_evaluation.h>
-
 using namespace dealii;
 
 template <int dim, int fe_degree, std::floating_point NumberType>
