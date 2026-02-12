8128b5196f9d2d20f8b82325e13b79727a34466f
GiorgioMantoan <232375862+GiorgioMantoan02@users.noreply.github.com>
2026-02-12 10:44:33 +0100

    ADR standard implementation

---
+++ ADR-Matrix-implementation/Parallel_version/src/DiffusionReaction-parallel.hpp
@@ -0,0 +1,165 @@
+#ifndef DIFFUSION_REACTION_HPP
+#define DIFFUSION_REACTION_HPP
+
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/distributed/fully_distributed_tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_simplex_p.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_fe.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+
+/**
+ * Class managing the differential problem.
+ */
+class DiffusionReactionParallel
+{
+public:
+  // Physical dimension (1D, 2D, 3D)
+  static constexpr unsigned int dim = 2;
+  class FunctionG : public Function<dim>
+  {
+  public:
+    // Constructor.
+    FunctionG()
+    {
+    }
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> &p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return std::sin(3.0 * M_PI * p[0]) + std::sin(2.0 * M_PI * p[1]);
+    }
+  };
+
+  // Constructor.
+  DiffusionReactionParallel(const std::string &mesh_file_name_,
+                    const unsigned int &r_,
+                    const std::function<double(const Point<dim> &)> &mu_,
+                    const std::function<Tensor<1, dim>(const Point<dim> &)> &b_,
+                    const std::function<double(const Point<dim> &)> &sigma_,
+                    const std::function<double(const Point<dim> &)> &f_,
+                    const std::function<double(const Point<dim> &)> &phi_)
+    : mesh_file_name(mesh_file_name_)
+    , r(r_)
+    , mu(mu_)
+    , b(b_)
+    , sigma(sigma_)
+    , f(f_)
+    , phi(phi_)
+    , mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD))
+    , mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD))
+    , mesh(MPI_COMM_WORLD)
+    , pcout(std::cout, mpi_rank == 0)
+  {}
+
+  // Initialization.
+  void
+  setup();
+
+  // System assembly.
+  void
+  assemble();
+
+  // System solution.
+  void
+  solve();
+
+  // Output.
+  void
+  output() const;
+
+  // Compute the error against a given exact solution.
+  double
+  compute_error(const VectorTools::NormType &norm_type,
+                const Function<dim>         &exact_solution) const;
+
+protected:
+  // Name of the mesh.
+  const std::string mesh_file_name;
+
+  // Polynomial degree.
+  const unsigned int r;
+
+  // Diffusion coefficient.
+  std::function<double(const Point<dim> &)> mu;
+
+  // Bterm.
+  std::function<Tensor<1, dim>(const Point<dim> &)> b;
+
+  // Reaction coefficient.
+  std::function<double(const Point<dim> &)> sigma;
+
+  // Forcing term.
+  std::function<double(const Point<dim> &)> f;
+
+  // Phi term.
+  std::function<double(const Point<dim> &)> phi;
+
+  // Number of MPI processes.
+  const unsigned int mpi_size;
+
+  // Rank of the current MPI process.
+  const unsigned int mpi_rank;
+
+  // Triangulation. The parallel::fullydistributed::Triangulation class manages
+  // a mesh that is completely distributed across all MPI processes (i.e. each
+  // process only stores its own locally relevant cells).
+  parallel::fullydistributed::Triangulation<dim> mesh;
+
+  // Finite element space.
+  std::unique_ptr<FiniteElement<dim>> fe;
+
+  // Quadrature formula.
+  std::unique_ptr<Quadrature<dim>> quadrature;
+
+  // Quadrature formula for boundary integrals.
+  std::unique_ptr<Quadrature<dim - 1>> quadrature_boundary;
+
+  // DoF handler.
+  DoFHandler<dim> dof_handler;
+
+  // System matrix.
+  TrilinosWrappers::SparseMatrix system_matrix;
+
+  // System right-hand side.
+  TrilinosWrappers::MPI::Vector system_rhs;
+
+  // System solution.
+  TrilinosWrappers::MPI::Vector solution;
+
+  // Output stream for process 0.
+  ConditionalOStream pcout;
+
+  // Locally owned DoFs for current process.
+  IndexSet locally_owned_dofs;
+};
+
+#endif
---
+++ ADR-Matrix-implementation/Parallel_version/src/exercise-01-parallel.cpp
@@ -0,0 +1,32 @@
+#include "DiffusionReaction-parallel.hpp"
+
+// Main function.
+int
+main(int argc, char *argv[])
+{
+  constexpr unsigned int dim = DiffusionReactionParallel::dim;
+
+  // This object calls MPI_Init when it is constructed and MPI_Finalize when it
+  // is destroyed. It also initializes several other libraries bundled with
+  // dealii (e.g. p4est, PETSc, ...).
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+
+  const std::string  mesh_filename = "../mesh/mesh-square-h0.100000.msh";
+  const unsigned int degree        = 2;
+
+
+  const auto mu = [](const Point<dim> &/*p*/) { return 1.0;};
+  const auto b = [](const Point<dim> &/*p*/) { return Tensor<1, dim>({-1.0, -1.0}); };
+  const auto f     = [](const Point<dim>     &/*p*/) { return 0.0; };
+  const auto sigma = [](const Point<dim> & /*p*/) { return 1.0; };
+  const auto phi   = [](const Point<dim> &/*p*/) { return 1.0; };
+  
+  DiffusionReactionParallel problem(mesh_filename, degree, mu, b, sigma, f, phi);
+
+  problem.setup();
+  problem.assemble();
+  problem.solve();
+  problem.output();
+
+  return 0;
+}
