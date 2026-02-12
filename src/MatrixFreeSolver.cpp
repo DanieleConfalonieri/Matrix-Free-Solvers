77b263f6276735356f772947ba8c8dda597172d7
Chris <cristianocorona2424@gmail.com>
2026-02-12 23:32:28 +0000

    opt: output print for solve more detailed (MatrixFreeSolver)

--- src/MatrixFreeSolver.cpp
+++ src/MatrixFreeSolver.cpp
@@ -242,6 +242,7 @@ void MatrixFreeSolver<dim, fe_degree, NumberType>::solve()
 {
   Timer time;
   pcout << "Solving linear system..." << std::endl;
+  unsigned int n_iter = 0; 
   {
     // Compute the diagonal approximation for preconditioning
     system_matrix.compute_diagonal();
@@ -295,17 +296,32 @@ void MatrixFreeSolver<dim, fe_degree, NumberType>::solve()
             << std::endl;
       throw;
     }
-    
+    n_iter = solver_control.last_step();
+
     // Distribute Dirichlet values to the solution vector
     constraints.distribute(solution);
-    
-    // Stats
-    pcout << "   Solved in " << solver_control.last_step()
-          << " iterations." << std::endl;
   }
-  setup_time += time.wall_time();
-  time_details << "Solve linear system       (CPU/wall) " << time.cpu_time()
-               << "s/" << time.wall_time() << 's' << std::endl;
+
+  const double wall_time = time.wall_time();
+  const double cpu_time = time.cpu_time();
+  setup_time += wall_time;
+  
+  // Performance Metrics
+  const double time_per_iter = wall_time / n_iter;
+  
+  // Throughput (Weak Scaling)
+  const double throughput_mdofs = dof_handler.n_dofs() / time_per_iter / 1e6; // MDoFs/s
+
+  // Standard Output
+  pcout << "   Solved in " << n_iter << " iterations." << std::endl;
+
+  // Detailed Output (for profiling)
+  time_details << "Solve linear system       (CPU/wall) " << cpu_time
+               << "s/" << wall_time << 's' << std::endl;
+               
+  time_details << "   iterations             " << n_iter << std::endl;
+  time_details << "   avg time/iter (wall)   " << time_per_iter << " s" << std::endl;
+  time_details << "   throughput             " << throughput_mdofs << " MDoFs/s" << std::endl;
 }
 
 /**
