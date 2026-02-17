eda137386fd17ee8baf8f109f11798d38f46d1ef
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2026-02-17 13:48:41 +0000

    feat:scratching data for matrix_based solver

--- build/run_profiling.sh
+++ build/run_profiling.sh
@@ -62,16 +62,22 @@ for p in {1..6}; do
 
         OUTPUT=$(mpirun -n $CORES $EXEC_MB 1 $L "$MULTIGRID_OPTION" $p)
 
-        
+        SETUP_SYSTEM_CPU=$(echo "$OUTPUT" | grep -oP 'Setup system\s*\(CPU\/wall\)\s\K[0-9.]+')
+        SETUP_SYSTEM_WALL=$(echo "$OUTPUT" | grep -oP 'Setup system\s*\(CPU\/wall\)\s[0-9.]+\s*s\/\s*\K[0-9.]+')
+        NUMBER_ELEMENTS=$(echo "$OUTPUT" | grep -oP 'Number of elements = \K[0-9]+')
         DOFS=$(echo "$OUTPUT" | grep -oP 'Number of degrees of freedom:\s*\K\d+')
+        DOFS_CELL=$(echo "$OUTPUT" | grep -oP 'DoFs per cell\s*=\s\K[0-9]+')
+        QPOINTS_CELL=$(echo "$OUTPUT" | grep -oP 'Quadrature points per cell\s*=\s\K[0-9]+')
         LINEAR_SYSTEM=$(echo "$OUTPUT" | grep -oP 'Solve linear system\s*\(CPU\/wall\)\s*\K[0-9.]+')
         ITERS=$(echo "$OUTPUT" | grep -oP 'Solved in\s*\K\d+')
         TIME_ITER=$(echo "$OUTPUT" | grep -oP 'Time per iter:\s*\K[0-9.]+')
         THROUGHPUT=$(echo "$OUTPUT" | grep -oP 'Throughput:\s*\K[0-9.]+\s*MDoFs/s')
+        ASSEMBLE_CPU=$(echo "$OUTPUT" | grep -oP 'Assembly complete\s*\(CPU\/wall\)\s*\K[0-9.]+')
+        ASSEMBLE_WALL=$(echo "$OUTPUT" | grep -oP 'Assembly complete\s*\(CPU\/wall\)\s*[0-9.]+s\/\K[0-9.]+')
         
 
         if [ ! -z "$DOFS" ] && [ ! -z "$THROUGHPUT" ] && [ ! -z "$ITERS" ] && [ ! -z "$TIME_ITER" ]; then
-            echo "MatrixBased,$p,$L,$NUMBER_ELEMENTS,$DOFS,$LINEAR_SYSTEM,$ITERS,$TIME_ITER,$THROUGHPUT" >> $CSV_FILE
+            echo "MatrixBased,$p,$L,$SETUP_SYSTEM_CPU,$SETUP_SYSTEM_WALL,$NUMBER_ELEMENTS,$DOFS,$DOFS_CELL,$QPOINTS_CELL,$LINEAR_SYSTEM,$ITERS,$TIME_ITER,$THROUGHPUT,$ASSEMBLE_CPU,$ASSEMBLE_WALL" >> $CSV_FILE
         else
             echo "   [!] Fallito o OOM per p=$p, Level=$L"
         fi
