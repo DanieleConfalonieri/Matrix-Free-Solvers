976dba5234d3825019ed184920d521e9dea069b2
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2026-02-18 10:24:55 +0000

    update:run_profiling_2

--- build/run_profiling_2.sh
+++ build/run_profiling_2.sh
@@ -7,20 +7,22 @@ EXEC_MB="./profiling_based"
 MULTIGRID_OPTION="-mg"
 
 MAX_LEVEL_MF=4
-N_CORES=56
+N_CORES=4
 
 # Definizione lista core: 1, e poi da 2 a 56 con passo 2"
-
+CORES_LIST="$(seq 2 2 56)"
 # Intestazione CSV (Aggiunta colonna Cores all'inizio)
 echo "Cores,Solver,Degree,Refinement,SetupSystemCPU,SetupSystemWall,NumberOfElements,DoFs,DoFsPerCell,QPointsPerCell,RhsCpu,RhsWall,SolvingLinearSystemCPU,SolvingLinearSystemWall,Iterations,TimePerIter(s),Throughput(MDoFs/s),OutputResultCpu,OutputResultWall" > $CSV_FILE
 
 # --- Matrix-Free Benchmark ---
 echo "=== Profiling Matrix-Free ==="
-    for p in {1..12}; do
-        for L in $(seq 4 $MAX_LEVEL_MF); do
-            echo "   Executing Matrix-Free (Cores=$N_CORES, p=$p, Level=$L)..."
+for n_cores in $CORES_LIST; do
+    echo "--- Running with $n_cores cores ---"
+    for p in {1..6}; do
+        for L in $(seq 3 $MAX_LEVEL_MF); do
+            echo "   Executing Matrix-Free (Cores=$n_cores, p=$p, Level=$L)..."
 
-            OUTPUT=$(mpirun -n $N_CORES $EXEC_MF 1 $L "$MULTIGRID_OPTION" $p)
+            OUTPUT=$(mpirun -n $n_cores $EXEC_MF 1 $L "$MULTIGRID_OPTION" $p)
 
             # Estrazione dati (Regex invariate)
             SETUP_SYSTEM_CPU=$(echo "$OUTPUT" | grep -oP 'Setup matrix-free system\s*\(CPU\/wall\)\s\K[0-9.]+')
@@ -36,16 +38,17 @@ echo "=== Profiling Matrix-Free ==="
             ITERS=$(echo "$OUTPUT" | grep -oP 'Solved in\s*\K\d+')
             TIME_ITER=$(echo "$OUTPUT" | grep -oP 'Time per iter:\s*\K[0-9.]+')
             THROUGHPUT=$(echo "$OUTPUT" | grep -oP 'Throughput:\s*\K[0-9.]+\s*MDoFs/s')
-            OUTPUT_RES_CPU=$(echo "$OUTPUT" | grep -oP 'Output')
-            OUTPUT_RES_WALL=$(echo "$OUTPUT" | grep -oP 'results\s*\(CPU\/wall\)\s*[0-9.]+s\/\K[0-9.]+')
+            #OUTPUT_RES_CPU=$(echo "$OUTPUT" | grep -oP 'Output')
+            #OUTPUT_RES_WALL=$(echo "$OUTPUT" | grep -oP 'results\s*\(CPU\/wall\)\s*[0-9.]+s\/\K[0-9.]+')
 
             if [ ! -z "$DOFS" ] && [ ! -z "$THROUGHPUT" ]; then
                 # Aggiunta n_cores come primo campo
-                echo "$N_CORES,MatrixFree,$p,$L,$SETUP_SYSTEM_CPU,$SETUP_SYSTEM_WALL,$NUMBER_ELEMENTS,$DOFS,$DOFS_CELL,$QPOINTS_CELL,$RHS_CPU,$RHS_WALL,$LINEAR_SYSTEM_CPU,$LINEAR_SYSTEM_WALL,$ITERS,$TIME_ITER,$THROUGHPUT,$OUTPUT_RES_CPU,$OUTPUT_RES_WALL" >> $CSV_FILE
+                echo "$n_cores,MatrixFree,$p,$L,$SETUP_SYSTEM_CPU,$SETUP_SYSTEM_WALL,$NUMBER_ELEMENTS,$DOFS,$DOFS_CELL,$QPOINTS_CELL,$RHS_CPU,$RHS_WALL,$LINEAR_SYSTEM_CPU,$LINEAR_SYSTEM_WALL,$ITERS,$TIME_ITER,$THROUGHPUT" >> $CSV_FILE
             else
-                echo "   [!] Fallito o OOM per Cores=$N_CORES, p=$p, Level=$L"
+                echo "   [!] Fallito o OOM per Cores=$n_cores, p=$p, Level=$L"
             fi
         done
     done
+done
 
 echo "=== Benchmark completed! Results saved in $CSV_FILE ==="
